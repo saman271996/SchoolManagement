170aa719d4e6ac87f74131bb6252a9ee4f51b58a
Aman Sharma <s.aman@singhsoft.com>
2024-08-02 13:56:12 +0530

    code commits

---
+++ SchoolManagement/Model/ClassViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Model
+{
+    public class ClassViewModel
+    {
+        public int Id { get;set; }
+        public int ClassId { get; set; }
+        public string SectionId { get; set; }
+        public int StreamId { get; set; }
+        public string ClassName { get; set; }
+        public string SectionName { get; set; }
+        public string StreamName { get; set; }
+    }
+}
